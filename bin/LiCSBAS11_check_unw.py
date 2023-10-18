8e1e3aba850813fd82584bd780deddfb3302e188
espiritocz <espiritocz@gmail.com>
2023-10-18 10:38:39 +0100

    Update LiCSBAS11_check_unw.py
    bug fix

--- bin/LiCSBAS11_check_unw.py
+++ bin/LiCSBAS11_check_unw.py
@@ -196,7 +196,7 @@ def main(argv=None):
             backupsfolder = os.path.dirname(unwfile)[:-18]+'.backup'
             if not os.path.exists(backupsfolder):
                 os.mkdir(backupsfolder)
-            shutil.move(os.path.dirname(unwfile), os.path.join(backupsfolder, ifgd)
+            shutil.move(os.path.dirname(unwfile), os.path.join(backupsfolder, ifgd))
 
     #%% Read date and network information
     ### Get dates
