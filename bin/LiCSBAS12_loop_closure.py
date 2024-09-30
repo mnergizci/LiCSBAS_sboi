26c5a8df8da686a990848bfabcf7ce24261bc756
espiritocz <espiritocz@gmail.com>
2024-09-30 21:37:15 +0100

    bugfix: avg phase loop closure (vartype error)

--- bin/LiCSBAS12_loop_closure.py
+++ bin/LiCSBAS12_loop_closure.py
@@ -649,7 +649,7 @@ def main(argv=None):
             print('{}'.format(i), file=f)
 
     # %% 4th loop to be used to calc n_loop_err and n_ifg_noloop
-    print('\n4th loop to compute statistics and remove pixels with loop errors...', flush=True)
+    print('\n4th loop to compute statistics (and remove pixels with loop errors if nullify was set)', flush=True)
     print('with {} parallel processing...'.format(_n_para2), flush=True)
 
     # create 3D cube - False means presumed error in the loop
@@ -685,6 +685,8 @@ def main(argv=None):
 
     # generate loop pngs:
     if do_pngs:
+        print('')
+        print('Generating PNG previews')
         ### Parallel processing
         p = q.Pool(_n_para)
         p.map(generate_pngs, range(n_loop))
@@ -1231,7 +1233,7 @@ def loop_closure_4th(args, da):
     one_array = np.ones((length, width), dtype=np.int8)
     loop_ph_wrapped_sum = np.zeros((length, width), dtype=np.float32)
     loop_ph_wrapped_sum_abs = np.zeros((length, width), dtype=np.float32)
-    nonan_count = np.zeros((length, width), dtype=np.int8)
+    nonan_count = np.zeros((length, width), dtype=np.float32) # although int16 would also do if needed..
     for i in range(i0, i1):
         if np.mod(i, 100) == 0:
             print("  {0:3}/{1:3}th loop...".format(i, n_loop), flush=True)
@@ -1256,7 +1258,7 @@ def loop_closure_4th(args, da):
         ns_loop_all.loc[:, :, ifgd23] = ns_loop_all.loc[:, :, ifgd23] + one_array_loop
         ns_loop_all.loc[:, :, ifgd13] = ns_loop_all.loc[:, :, ifgd13] + one_array_loop
         ## Count number of loops with suspected unwrap error (by default >pi)
-        nonan_count = nonan_count + (1 * (~np.isnan(loop_ph))).astype(np.int8)
+        nonan_count = nonan_count + (1 * (~np.isnan(loop_ph))) #.astype(np.int8)
         loop_ph[np.isnan(loop_ph)] = 0  # to avoid warning
         ## Summing the phase closure values -> will get average (wrapped) phase
         loop_ph_wrapped_sum = loop_ph_wrapped_sum + np.angle(np.exp(1j * loop_ph))
@@ -1282,6 +1284,9 @@ def loop_closure_4th(args, da):
     title = 'Average phase loop closure error (abs)'
     #plot_lib.make_im_png(loop_ph_wrapped_sum_abs/n_loop, file + '.png', cmap_noise_r, title)
     plot_lib.make_im_png(loop_ph_avg_abs, file + '.png', cmap_noise_r, title)
+
+    del nonan_count, loop_ph_wrapped_sum_abs, loop_ph_wrapped_sum
+
     #
     # Lin Shen update ---- WARNING / TODO: 
     print('Updating the loop closure error matrix - identifying (in)correct ifgs')
